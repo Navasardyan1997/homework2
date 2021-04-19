47300f183eb14e77dbef0f1708da138b164b6e68
Navasardyan1997 <82539980+Navasardyan1997@users.noreply.github.com>
2021-04-20 00:24:46 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+#N1
+num=12345
+while num!=0:
+    a=num%10
+    b=a
+    num=num//10
+    print(b)
+
+
+#N2
+str1="programming"
+str2=len(str1)
+print(str2)
+
+#N3
+P=6
+for i in range(2, P):
+    x = P % i
+    if x == 0:
+        print("NOT")
+        break
+print("PRIME")
+
+
+
+
+
+
+#N4
+for a in range(1,6):
+    print(a,"000000")
+
+#N5
+a=10
+b=6
+c=4
+if a+b==c or a+c==b or b+c==a:
+    print("yes")
+else:
+    print("no")
+
+#N6
+for a in range (0,15):
+    num+=a
+print(num)
+
+
+
+
+
+
